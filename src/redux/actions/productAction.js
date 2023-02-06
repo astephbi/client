b9532ee020a5873069c73b7908a8790c883182a3
Alex <astephbi@gmail.com>
2023-02-06 09:44:46 +0000

    first part frontend done

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 import { setProducts, setLoading, setError } from '../slices/product';
 
-export const getProducts = () => async (dispacth) => {
-  dispacth(setLoading(true));
+export const getProducts = () => async (dispatch) => {
+  dispatch(setLoading(true));
+
   try {
-  } catch (date) {}
+    const { data } = await axios.get('/api/products');
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : 'An unexpected error has occured'
+      )
+    );
+  }
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { combineReducer, configureStore } from '@reduxjs/toolkit';
+import products from '../slices/product';
 
-const reducer = combineReducer({});
+const reducer = combineReducer({
+  products,
+});
 export default configureStore({
   reducer,
 });
